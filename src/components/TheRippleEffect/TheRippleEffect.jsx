66969beb0f1f9cde792a960e8a5cbdc8aa978ee0
Michael LaFond <Mike24mn@gmail.com>
2024-07-26 16:25:33 -0500

    resolved conflicts related to the logo ripple effect and database within the pool.js

--- src/components/TheRippleEffect/TheRippleEffect.jsx
+++ src/components/TheRippleEffect/TheRippleEffect.jsx
@@ -1,11 +1,12 @@
 
 import React from 'react';
 import './TheRippleEffect.css';
-import logo from './public/image copy.png'
+import logo from '../../../../MockHeyDay/public/image copy.png'
+
 function TheRippleEffect() {
   return (
     <div className="loader-container">
-      
+      <img src={logo} className="logo" />
       <div className="ripple"></div>
     </div>
   );
