9a2cae9791e671ba83e3bd3d457fca9736a99396
Kalia Vang <kaliavang511@gmail.com>
2024-08-01 10:52:53 -0500

    wildcard & favorites

--- src/components/TheRippleEffect/TheRippleEffect.jsx
+++ src/components/TheRippleEffect/TheRippleEffect.jsx
@@ -1,12 +1,12 @@
 
 import React from 'react';
 import './TheRippleEffect.css';
-// import logo from '../../../../MockHeyDay/public/image copy.png'
+import logo from '../../../../MockHeyDay/public/image copy.png'
 
 function TheRippleEffect() {
   return (
     <div className="loader-container">
-      {/* <img src={logo} className="logo" /> */}
+      <img src={logo} className="logo" />
       <div className="ripple"></div>
     </div>
   );
